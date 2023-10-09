b4840db8daa5f38a67915231c01ba28973bf3d7f
SahanPunchihewa <sahannilupul18@gmail.com>
2023-10-10 00:13:05 +0530

    Project Setup

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <>
+        
+        <h1 className="text-3xl font-bold">Home Page</h1>
+        
+        </>
+    );
+
+
+};
+
+export default Home;
