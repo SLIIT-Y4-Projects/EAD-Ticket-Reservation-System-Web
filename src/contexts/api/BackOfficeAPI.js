d6122cabb52f29c99182877896e48d9db3d5850e
SahanPunchihewa <sahannilupul18@gmail.com>
2023-10-12 15:47:42 +0530

    Back Officer API

--- src/contexts/api/BackOfficeAPI.js
+++ src/contexts/api/BackOfficeAPI.js
@@ -8,7 +8,23 @@ class BackOfficeAPI {
 
     static login(values) {
 
-        return axios.post()
+        return axios.post(`${BASE_URL}/api/BackOfficeUser/login/`, values, requestConfigJson);
+    }
+
+    static getAll(){
+        return axios.get(`${BASE_URL}/api/BackOfficeUser/`, requestConfig);
+    }
+
+    static getOne(id){
+        return axios.get(`${BASE_URL}/api/BackOfficeUser/${id}`, requestConfig);
+    }
+
+    static edit(id, values){
+        return axios.put(`${BASE_URL}/api/BackOfficeUser/${id}`, values, requestConfigJson);
+    }
+
+    static delete(id) {
+
     }
 
 }
