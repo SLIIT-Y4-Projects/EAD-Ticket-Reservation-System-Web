89a67a93708c19182554632c8799742850b748fa
Sahan Punchihewa <sahannilupul18@gmail.com>
2023-10-13 22:02:48 +0530

    Train Management

--- src/contexts/TrainContext.jsx
+++ src/contexts/TrainContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import TrainAPI from "./api/TrainAPI";
 import makeToast from "../components/toast";
 
@@ -6,16 +6,111 @@ const TrainContext = createContext();
 
 export function TrainProvider({ children }) {
 
+    const [trains, setTrains] = useState([]);
 
+    const [train, setTrain] = useState({
+        trainNumber: "",
+        departureTime: "",
+        capacity: "",
+        status: ""
+    });
+
+    // Insert new train
+    const createTrain = async (newTrain) => {
+        try {
+            const response = await TrainAPI.createTrain(newTrain);
+            setTrains([...trains, response.data]);
+            makeToast({ type: "success", message: "Train inserted successful" });
+            window.location.href = ""
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // get all train details
+    useEffect(() => {
+        TrainAPI.getAll().then((response) => {
+            setTrains(response.data);
+        });
+    }, []);
+
+    //get one train details
+    const getOne = (id) => {
+        useEffect(() => {
+            TrainAPI.getOne(id).then((response) => {
+                setTrain(response.data);
+            });
+        }, []);
+    };
+
+    // delete train
+    const deleteTrain = (id) => {
+        TrainAPI.deleteTrain(id).then(() => {
+            setTrains(trains.filter((trains) => trains.id !== id));
+            makeToast({ type: "success", message: "Train Deleted Successful" });
+            window.location.href = ""
+        });
+    };
+
+    // active train 
+
+    const activeTrain = (id) => {
+        console.log("id--", id)
+        TrainAPI.activeTrain(id).then((response) => {
+            makeToast({ type: "success", message: "Train Activated" });
+            window.location.href = ""
+        })
+            .catch((err) => {
+
+                console.log(err);
+            })
+    };
+
+    // Cancel Train
+    const cancelTrain = (id) => {
+        console.log("id--", id)
+        TrainAPI.cancelTrain(id).then((response) => {
+            makeToast({ type: "success", message: "Train Canceled" });
+            window.location.href = ""
+        })
+            .catch((err) => {
+
+                console.log(err);
+            });
+    }
+
+    // Edit train details
+    const editTrain = (values) => {
+        const newTrain = {
+            trainNumber: values.trainNumber,
+            departureTime: values.departureTime,
+            capacity: values.capacity,
+            status: values.status,
+        };
+        TrainAPI.editTrain(values.id, newUser)
+            .then((response) => {
+                makeToast({ type: "success", message: "Train Updated Successful" });
+                window.location.href = ""
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
 
     return (
 
         <TrainContext.Provider
             value={{
-
-
-
+                train,
+                setTrain,
+                trains,
+                setTrains,
+                createTrain,
+                deleteTrain,
+                activeTrain,
+                editTrain,
             }}
 
         >
@@ -23,10 +118,7 @@ export function TrainProvider({ children }) {
             {children}
 
         </TrainContext.Provider>
-
-
     );
-
 };
 
 export default TrainContext;
--- src/contexts/api/TrainAPI.js
+++ src/contexts/api/TrainAPI.js
@@ -2,11 +2,44 @@ import axios from "axios";
 import requestConfig from "./requestConfig";
 import requestConfigJson from "./requestConfigJson";
 
+// backend env file calling
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 class TrainAPI {
 
-    
+    // Create Train
+    static create (values) {
+
+        return axios.post(`${BASE_URL}/api/Train/`, values, requestConfigJson)
+    }
+
+    // Get all Train details
+    static getAll(){
+        return axios.get(`${BASE_URL}/api/Train/`, requestConfig)
+    }
+
+    // get one train details
+    static getOne(id){
+        return axios.get(`${BASE_URL}/api/Train/${id}`,id, requestConfig)
+    }
+    // edit train details
+    static edit(id, values) {
+        return axios.put(`${BASE_URL}/api/Train/${id}`, values, requestConfigJson)
+    }
+    // activate train
+    static activeTrain(id, values){
+        return axios.put(`${BASE_URL}/api/Train/activate/${id}`, values, requestConfigJson)
+    }
+
+    // cancel train
+    static cancelTrain(id, values) {
+        return axios.put(`${BASE_URL}/api/Train/cancel/${id}`, values, requestConfigJson)
+    }
+
+    //delete train 
+    static deleteTrain(id) {
+        return axios.delete(`${BASE_URL}/api/Train/${id}`, requestConfig)
+    }
 
 }
 
